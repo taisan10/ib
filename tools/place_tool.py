872c80b759b8d0ef89bc4abe2659a8b699d522d1
taisan10 <tarunbansalsgnr@gmail.com>
2026-05-28 23:12:23 +0530

    code

--- tools/place_tool.py
+++ tools/place_tool.py
@@ -1,28 +1,75 @@
 
+# from langchain.tools import tool
+
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from langchain_community.vectorstores import FAISS
+# import os
+# # IMPORTANT
+# from rag.data.travel_data import travel_data
+
+
+# embeddings = GoogleGenerativeAIEmbeddings( model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY") )
+
+# db = FAISS.load_local(
+#     "faiss_index",
+#     embeddings,
+#     allow_dangerous_deserialization=True
+# )
+
+# retriever = db.as_retriever(
+#     search_kwargs={"k":1}
+# )
+
+
+# @tool
+# def search_places(city:str)->str:
+
+#     """
+#     Travel guide information
+#     """
+
+#     data = travel_data.get(city)
+
+#     if data:
+
+#         return f"""
+# {city}
+
+# Best Season:
+# {data['season']}
+
+# Recommended Days:
+# {data['recommended_days']}
+
+# Places:
+# {chr(10).join(data['places'])}
+
+# Food:
+# {chr(10).join(data['food'])}
+
+# Travel Tips:
+# {chr(10).join(data['tips'])}
+# """
+
+#     # fallback FAISS
+#     docs = retriever.invoke(
+#         f"best places in {city}"
+#     )
+
+#     result="\n".join(
+#         [doc.page_content for doc in docs]
+#     )
+
+#     return result
+
+
 from langchain.tools import tool
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import FAISS
-import os
-# IMPORTANT
 from rag.data.travel_data import travel_data
 
 
-embeddings = GoogleGenerativeAIEmbeddings( model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY") )
-
-db = FAISS.load_local(
-    "faiss_index",
-    embeddings,
-    allow_dangerous_deserialization=True
-)
-
-retriever = db.as_retriever(
-    search_kwargs={"k":1}
-)
-
-
 @tool
-def search_places(city:str)->str:
+def search_places(city: str) -> str:
 
     """
     Travel guide information
@@ -51,13 +98,4 @@ Travel Tips:
 {chr(10).join(data['tips'])}
 """
 
-    # fallback FAISS
-    docs = retriever.invoke(
-        f"best places in {city}"
-    )
-
-    result="\n".join(
-        [doc.page_content for doc in docs]
-    )
-
-    return result
+    return f"No travel data available for {city}"
