211508f413e314f63f2f96e97fbc8a8ff8e6a2c3
taisan10 <tarunbansalsgnr@gmail.com>
2026-06-05 15:25:48 +0530

    Fix line endings with .gitattributes

--- api/app.py
+++ api/app.py
@@ -137,7 +137,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+      allow_origins=[
+       
+        "https://ai-tour-guider.vercel.app/",  
+        
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
